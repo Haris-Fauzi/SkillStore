59779e51f171111dec0c8e682d5363d67cffeef3
Haris Fauzi <haris.fauzi.g@gmail.com>
2026-07-02 17:56:01 +0700

    fix: move artisan cache to runtime entrypoint

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
-// api/index.php
+
+// Jalankan optimasi routing & config secara otomatis saat aplikasi pertama kali diakses
+exec('php ../artisan config:cache');
+exec('php ../artisan route:cache');
+
+// Pintu masuk utama Laravel
 require __DIR__ . '/../public/index.php';
