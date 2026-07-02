a95d599ac353c01ab2136d5490873d8d237864ef
Haris Fauzi <haris.fauzi.g@gmail.com>
2026-07-02 18:13:31 +0700

    fix: remove exec cache to fix 500 error

--- api/index.php
+++ api/index.php
@@ -1,8 +1,4 @@
 <?php
 
-// Jalankan optimasi routing & config secara otomatis saat aplikasi pertama kali diakses
-exec('php ../artisan config:cache');
-exec('php ../artisan route:cache');
-
-// Pintu masuk utama Laravel
+// Pintu masuk utama Laravel ke folder public
 require __DIR__ . '/../public/index.php';
